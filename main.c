69d6e427e045c73824c58943acda533e9ba37b08
Ondrej Nentvich <Ondrej Nentvich>
2017-01-11 20:54:02 +0100

    print Hello PRP

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+	Author: Ondrej Nentvich
+	E-mail: nentvond@fel.cvut.cz
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char**argv) {
+    
+    printf("Hello PRP\n");
+    return 0;
+}
